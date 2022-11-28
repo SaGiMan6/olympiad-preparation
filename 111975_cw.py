77516e041d51035a67acefea4e6e960b2eb2ff6a
Sagiman <sasha@sagiman.ru>
2022-11-28 14:35:57 +0700

    111975_c_37

--- 111975_cw.py
+++ 111975_cw.py
@@ -4,18 +4,20 @@ a = [int(x) for x in input().split(" ")]
 
 
 v = 0
-x = 0
-x1 = x
 y = n - 1
+x = y - 1
 
 
-while x < y:
+while y >= 1:
     if a[y] - a[x] > r:
-        v += 1
-        x += 1
+        v += x + 1
+        y -= 1
+        x = y - 1
+    elif x == 0:
+        y -= 1
+        x = y - 1
     else:
-        x = 0
-        y = y - 1
+        x -= 1
 
 
 print(v)
