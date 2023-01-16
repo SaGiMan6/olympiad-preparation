9b5fd7e868eb25f23e7b549bfb9507c09c8a77fa
Sagiman <sasha@sagiman.ru>
2023-01-16 12:58:19 +0700

    lin

--- 2_c.py
+++ 2_c.py
@@ -26,3 +26,25 @@ for i in range(k):
             print(a[l])
         elif (a[r] - b[i]) < (b[i] - a[l]):
             print(a[r])
+
+
+n, m = map(int, input().split())
+
+a = [[] for j in range(n)]
+
+for i in range(n):
+    a[i] = list(map(int, input().split()))
+
+
+mxc = a[0][0]
+mxi = [0, 0]
+
+
+for i in range(n):
+    for j in range(m):
+        if a[i][j] > mxc:
+            mxc = a[i][j]
+            mxi = [i, j]
+
+
+print(str(mxi[0]) + " " + str(mxi[1]))
