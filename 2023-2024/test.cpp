b0b6ee0379c2e7dd0296b637442af983d8dd75ad
Sagiman <sasha@sagiman.ru>
2023-09-03 17:54:04 +0700

    some solutions

---
+++ 2023-2024/test.cpp
@@ -0,0 +1,12 @@
+/* std::string str_nums;
+
+	std::getline(std::cin, str_nums);
+	int len = str_nums.length();
+
+	std::string temp;
+	std::vector <int> nums;
+
+	for (int i = 0; i < len; i++) {
+		if
+	} */
+
---
+++ 2023-2024/tink_1D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::string commands[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> commands[i];
+    }
+
+
+}
